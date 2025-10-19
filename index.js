4746604ac098c0d436b8a66a3a33d8ccb746f1a2
jeetjapani <jeetjapani01+github@gmail.com>
2025-10-19 17:07:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,4 +25,5 @@ for (let line of startupMessage.split("\n")) {
 	console.log(line)
 }
 
-app.listen(port, () => app.listen(port, () => console.log(`sample-expressjs app listening on port ${port}!`))
+app.listen(port, () => app.listen(port, () => cconsole.log(`The current environment is ${process.env.NODE_ENV}`);
+
